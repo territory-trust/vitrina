fdbc2ea7361d3ce8b7bd2b5512dd7cd9dcd2a36e
territory-trust <i0985887331@gmail.com>
2026-01-17 00:39:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,13 @@ const CONFIG = {
 
     // ВСТАВЬТЕ ВАШ НОВЫЙ КЛЮЧ НИЖЕ
     AI_KEY: 'sk-or-v1-
-e11169f750fc236c6a6cd85a933f908696b38b6266f27b7cd106d3463675e2a2',
+
+e11169f750fc236c6a6cd85a933f908696b38b6266f27b7cd106
+
+d3463675e2a2',
     AI_MODEL: 'openai/gpt-4o-mini',
 
     AFFILIATE_ID: '111958', 
     NETWORK: 'SalesDoubler' 
 };
+
