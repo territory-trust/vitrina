c414728e2c697557fce0e32ef1c28f02382483c8
territory-trust <i0985887331@gmail.com>
2026-01-17 01:24:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,10 @@ const CONFIG = {
     SB_URL: 'https://udniuvrrlbeyivqqbjjw.supabase.co',
     SB_KEY: 'sb_publishable_W1xgtj3wLDplfGZb9_dwcw_OLxCLxX0',
 
-    // ВСТАВЬТЕ ВАШ НОВЫЙ КЛЮЧ НИЖЕ
-    AI_KEY: 'sk-or-v1-
-
-e11169f750fc236c6a6cd85a933f908696b38b6266f27b7cd106d3463675e2a2',
+    // Ваш новый ключ OpenRouter
+    AI_KEY: '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',
     AI_MODEL: 'openai/gpt-4o-mini',
 
     AFFILIATE_ID: '111958', 
     NETWORK: 'SalesDoubler' 
 };
-
-
