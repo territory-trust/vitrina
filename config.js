5c7738fd003a49e72a75bbc8b1e6bd716c11845a
territory-trust <i0985887331@gmail.com>
2026-01-16 23:24:21 +0200

    Новый ключ config.js

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
-// ГЛАВНЫЙ ЦЕНТР УПРАВЛЕНИЯ
 const CONFIG = {
-    // База данных
     SB_URL: 'https://udniuvrrlbeyivqqbjjw.supabase.co',
     SB_KEY: 'sb_publishable_W1xgtj3wLDplfGZb9_dwcw_OLxCLxX0',
 
-    // Ключ ИИ (Замените, если ошибка "User not found" останется)
-    AI_KEY: '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',
+    // ВСТАВЬТЕ ВАШ НОВЫЙ КЛЮЧ НИЖЕ
+    AI_KEY: 'sk-or-v1-
+e11169f750fc236c6a6cd85a933f908696b38b6266f27b7cd106d3463675e2a2',
     AI_MODEL: 'openai/gpt-4o-mini',
 
-    // Ваш кошелек SalesDoubler
     AFFILIATE_ID: '111958', 
     NETWORK: 'SalesDoubler' 
 };
