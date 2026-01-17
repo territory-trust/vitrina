05b50fecc1e76d7349b9710c12449d5f62ceb2f3
territory-trust <i0985887331@gmail.com>
2026-01-17 20:08:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,12 @@ const CONFIG = {
     SB_URL: 'https://udniuvrrlbeyivqqbjjw.supabase.co',
     SB_KEY: 'sb_publishable_W1xgtj3wLDplfGZb9_dwcw_OLxCLxX0',
 
-    // ВАШ ПОЛНЫЙ КЛЮЧ
+    // Ваш полный ключ ...662a
     AI_KEY: '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',
     
-    AI_MODEL: 'openai/gpt-4o-mini',
+    // МЕНЯЕМ МОДЕЛЬ НА БЕЗОТКАЗНУЮ
+    AI_MODEL: 'huggingfaceh4/zephyr-7b-beta:free',
+
     AFFILIATE_ID: '111958', 
     NETWORK: 'SalesDoubler' 
 };
