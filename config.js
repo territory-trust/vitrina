6c64fbc545ed45c28bfc8ad4ae0c0e706f348a38
territory-trust <i0985887331@gmail.com>
2026-01-17 20:26:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,9 @@ const CONFIG = {
     SB_URL: 'https://udniuvrrlbeyivqqbjjw.supabase.co',
     SB_KEY: 'sb_publishable_W1xgtj3wLDplfGZb9_dwcw_OLxCLxX0',
 
-    // Ваш полный ключ ...662a
-    AI_KEY: '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',
+    // ВСТАВЬТЕ СЮДА НОВЫЙ КЛЮЧ ОТ GOOGLE (AIza...)
+    GOOGLE_AI_KEY: 'AIzaSyBmunos8jmgxkVeG3Dxjey44or9E9voOYw',
     
-    // МЕНЯЕМ МОДЕЛЬ НА БЕЗОТКАЗНУЮ
-    AI_MODEL: 'huggingfaceh4/zephyr-7b-beta:free',
-
     AFFILIATE_ID: '111958', 
     NETWORK: 'SalesDoubler' 
 };
