9e1f9782101fe7231741ce3fca9d90af31266512
territory-trust <i0985887331@gmail.com>
2026-01-18 11:09:36 +0200

    4-й рабочий ключ (который с балансом $5) config.js

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ const CONFIG = {
     SB_KEY: 'sb_publishable_W1xgtj3wLDplfGZb9_dwcw_OLxCLxX0',
 
     // ВСТАВЬТЕ СЮДА НОВЫЙ КЛЮЧ ОТ GOOGLE (AIza...)
-    GOOGLE_AI_KEY: 'AIzaSyBmunos8jmgxkVeG3Dxjey44or9E9voOYw',
+    GOOGLE_AI_KEY: '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',
     
     AFFILIATE_ID: '111958', 
     NETWORK: 'SalesDoubler' 
 };
+
